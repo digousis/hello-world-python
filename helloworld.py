0e07163df3be6c7af58103cf4a43646d69c27016
Dimitrios Gousis <gousisdim@gmail.com>
2025-10-21 13:02:53 +0300

    main function added

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,13 @@
 # Author: Dimitrios Gousis
-
 # The hello-world code in python.
 
-print("Hello world")
+def execute_process() -> None:
+    """
+    Execute the process of this script.
+    :return:
+    """
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    execute_process()
